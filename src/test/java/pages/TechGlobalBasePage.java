a1cf8c4395e4ec741e96aadfb49841ea0ce278d4
Andrii Grechyn <gre4ya@icloud.com>
2023-02-05 11:52:44 -0600

    added techGlobal.feature file

---
+++ src/test/java/pages/TechGlobalBasePage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.util.List;
+
+public class TechGlobalBasePage {
+    public TechGlobalBasePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy(id = "logo")
+    public WebElement logo;
+    @FindBy(id = "dropdown-button")
+    public WebElement headerDropdown;
+    @FindBy(css = "#dropdown-menu a")
+    public List<WebElement> headerDropDownOptions;
+
+    @FindBy(css = ".Footer_footer__kXlYa p")
+    public List<WebElement> footerParagraphs;
+
+    @FindBy(css = ".Footer_footer__kXlYa a")
+    public List<WebElement> footerSocialLinks;
+
+
+}
--- src/test/java/steps/GoogleSteps.java
+++ src/test/java/steps/GoogleSteps.java
@@ -4,7 +4,6 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import pages.GoogleSearchPage;
