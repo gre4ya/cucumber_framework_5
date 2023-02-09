921e2ff7bed859811edab48b5b484ebbaaf8efb4
Andrii Grechyn <gre4ya@icloud.com>
2023-02-08 19:35:49 -0600

    added new lines of code to the cucumber_framework

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -8,7 +8,11 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "",
-        dryRun = false
+        tags = "@Login",
+        dryRun = false,
+        monochrome = false,
+        plugin = {"html:target/cucumber-reports",
+        "json:target/cucumber-reports/Cucumber.json"}
+
 )
 public class Runner {}
